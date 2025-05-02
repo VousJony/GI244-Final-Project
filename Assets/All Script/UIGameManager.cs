07768e9c380b66062d5c22a5014ffc50c79226db
Vous Jony <jonykingvip@gmail.com>
2025-05-02 20:13:01 +0700

    Update 0.8
    -Fix UI Defeat Score

--- Assets/All Script/UIGameManager.cs
+++ Assets/All Script/UIGameManager.cs
@@ -13,7 +13,8 @@ public class UIGameManager : MonoBehaviour
 
     [Header("UI Elements")]
     public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI scoreTextSummay;
+    public TextMeshProUGUI scoreTextSummary;
+    public TextMeshProUGUI scoreSummaryDefeat;
     public TextMeshProUGUI HealthText;
 
 
@@ -93,7 +94,8 @@ public class UIGameManager : MonoBehaviour
     {
         this.score += score;
         scoreText.text = this.score.ToString();
-        scoreTextSummay.text = this.score.ToString();
+        scoreTextSummary.text = this.score.ToString();
+        scoreSummaryDefeat.text = this.score.ToString();
     }
 
     public void DeleteScore(int Health)
